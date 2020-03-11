eee320a3eb70c2ca880061ce23f6f170b3a104b5
Drashty Batavia <DrashtyB@viitorcloud.local>
2020-03-11 18:13:30 +0530

    hooks implementation

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-import React, { useState } from "react";
-import "./App.css";
-import {makeStyles} from '@material-ui/core'
+import React, { useState, useEffect } from "react";
+import AppReducer from "./hook2";
+import { makeStyles } from "@material-ui/core";
+import Test from "./hook3";
 
-const useStyles = makeStyles(theme =>({
-  button: {
-    marginLeft: 20
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginLeft: 20,
+    marginTop: 10
   }
-}))
-function App({text, maxLength}) {
-  const classes = useStyles()
+}));
+function App({ text }) {
+  const classes = useStyles();
   const [display, setDisplay] = useState(true);
-  if (text === maxLength) {
-    return <p>{ text }</p>;
-  }
+  useEffect(() => {
+    console.log("useEffect hook");
+  }, [display]);
+
   return (
-    <div>
-      {display ? '{text.substr(0,0).trim()}': text}
-      <br/>
-      <div className={classes.button}>
-      {display ? (
-        <button onClick={() => setDisplay(false)}>Hide</button>
-      ) : (
-        <button onClick={() => setDisplay(true)}>Show</button>
-      )}
+    <div className={classes.root}>
+      {display ? `${text.substr(0, 0)}` : text}
+      <br />
+      <div>
+        {display ? (
+          <button onClick={() => setDisplay(false)}>Show</button>
+        ) : (
+          <button onClick={() => setDisplay(true)}>Hide</button>
+        )}
       </div>
+      <AppReducer/>
+      <Test/>
     </div>
   );
 }
 
 function Show() {
-  return <App text={'React hook'} maxLength={10}></App>;
+  return <App text={"useState hook"}/>;
 }
 export default Show;
