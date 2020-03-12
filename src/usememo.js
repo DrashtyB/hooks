f53329dc0268cf8fbdd6977bd56da2c0f569e1eb
DrashtyB <dnbatavia111@gmail.com>
2020-03-13 00:22:07 +0530

    modified

--- src/usememo.js
+++ src/usememo.js
@@ -5,14 +5,16 @@ function Memo(){
     const [count, setCount] = useState(0)
     const expFunc = (count) => {
         waitSync(18000)
-        return(count * 5)
+        return(count * 10)
     }
     const resCount = useMemo(()=> {
         return expFunc(count)
     },[count])
     return(
         <div>
+            <p>"useMemo:"</p>
             Count: {resCount}
+            <br/>
             <input type="text" onChange={e => setCount(e.target.value)} placeholder="Set count"/>
         </div>
     )
