dd08b1c7cffc2e1d9dd1fff7bb10402746e0898c
Drashty Batavia <DrashtyB@viitorcloud.local>
2020-03-13 18:41:15 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,43 +1,65 @@
-import React, { useState } from "react";
-import AppReducer from "./reducer";
-import { makeStyles } from "@material-ui/core";
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import AppContext from "./context";
 import Callback from "./callback";
 import Ref from "./ref";
+import Show from "./state";
+import Effect from "./Effect";
+import TodoReducer from "./TodoReducer";
 import Memo from "./usememo";
-import Effect from "./effect";
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    marginLeft: 20,
-    marginTop: 10
-  }
-}));
-function App({ text }) {
-  const classes = useStyles();
-  const [display, setDisplay] = useState(true);
-
+function App() {
   return (
-    <div className={classes.root}>
+    <Router>
       <div>
-        <p>"useState & useEffect demo"</p>
-        {display ? `${text.substr(0, 0)}` : text}
-        <br />
-        <button onClick={() => setDisplay(!display)}>
-          {display ? "Show" : "Hide"}
-        </button>
+        <ul>
+          <li>
+            <Link to="/useState">useState</Link>
+          </li>
+          <li>
+            <Link to="/useEffect">useEffect</Link>
+          </li>
+          <li>
+            <Link to="/useContext">useContext</Link>
+          </li>
+          <li>
+            <Link to="/useReducer">useReducer</Link>
+          </li>
+          <li>
+            <Link to="/useCallback">useCallback</Link>
+          </li>
+          <li>
+            <Link to="/useMemo">useMemo</Link>
+          </li>
+          <li>
+            <Link to="/useRef">useRef</Link>
+          </li>
+          <li>
+            <Link to="/useImperativeHandle">useImperativeHandle</Link>
+          </li>
+          <li>
+            <Link to="/useLayoutEffect">useLayoutEffect</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/useState" component={Show} />
+          <Route path="/useEffect" component={Effect} />
+          <Route path="/useContext" component={AppContext} />
+          <Route path="/useReducer" component={TodoReducer} />
+          <Route path="/useCallback" component={Callback} />
+          <Route path="/useMemo" component={Memo} />
+          <Route path="/useRef" component={Ref} />
+          <Route path="/useImperativeHandle" component={Ref} />
+          <Route path="/useLayoutEffect" component={Effect} />
+        </Switch>
       </div>
-      <Effect/>
+      {/* <Effect />
       <AppReducer />
       <AppContext />
       <Callback />
       <Ref />
-      <Memo />
-    </div>
+      <Memo /> */}
+    </Router>
   );
 }
-
-function Show() {
-  return <App text={"useState hook"} />;
-}
-export default Show;
+export default App;
