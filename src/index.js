535a71041984f1571e8782f10188d9842eff8774
Drashty Batavia <DrashtyB@viitorcloud.local>
2020-03-13 19:21:19 +0530

    import app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import App from "./state"
+import App from "./App"
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App/>, rootElement);
 
