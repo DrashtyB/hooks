03b32c8ab14f109cd3a84e55ebd8a9baa1589805
Drashty Batavia <DrashtyB@viitorcloud.local>
2020-03-13 13:22:49 +0530

    useMemo modified

--- src/usememo.js
+++ src/usememo.js
@@ -1,20 +1,31 @@
-import React, { useState, useMemo } from 'react'
+import React, { useState, useMemo } from "react";
 
-function Memo(){
-    const [count, setCount] = useState(0)
-    const multiplier = (count) => {
-        return(count * 10)
-    }
-    const memoCount = useMemo(()=> {
-        return multiplier(count)
-    },[count])
-    return(
-        <div>
-            <p>"useMemo:"</p>
-            Count: {memoCount}
-            <br/>
-            <input type="text" onChange={e => setCount(e.target.value)} />
-        </div>
-    )
+function Memo() {
+  const [count1, setCount1] = useState(null);
+  const [count2, setCount2] = useState(null);
+  const multiplier = (count1, count2) => {
+    return count1 * count2;
+  };
+  const memoCount = useMemo(() => {
+    return multiplier(count1, count2);
+  }, [count1, count2]);
+  console.log('memoCount:', memoCount)
+  return (
+    <div>
+      <p>"useMemo:"</p>
+      Count: {memoCount}
+      <br />
+      <input
+        type="text"
+        onChange={e => setCount1(e.target.value)}
+        placeholder="Count 1"
+      />
+      <input
+        type="text"
+        onChange={e => setCount2(e.target.value)}
+        placeholder="Count 2"
+      />
+    </div>
+  );
 }
-export default Memo
+export default Memo;
