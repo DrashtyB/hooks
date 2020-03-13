e55c57491d875c2896f7a124a16611c2af663808
Drashty Batavia <DrashtyB@viitorcloud.local>
2020-03-13 13:22:27 +0530

    memo removed

--- src/memo.js
+++ src/memo.js
@@ -5,20 +5,12 @@ const Count = (function Count({ count, onClick }) {
 });
 console.log("count", Count);
 
-// function Counter(count, onClick) {
-//   return useMemo(() => <button onClick={onClick}>{count}</button>, [
-//     count,
-//     onClick
-//   ]);
-// }
-
 function Memo() {
   const [state, setState] = useState(0);
   const add = useCallback(() => setState(state + 1), [state]);
   return (
     <div>
       <p>"useMemo demo</p>
-      {/* <Counter /> */}
       <Count count={state} onClick={add} />
     </div>
   );
