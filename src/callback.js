e5d891e2c89893180125931be5a9a5fd726acd84
Drashty Batavia <DrashtyB@viitorcloud.local>
2020-03-16 19:02:03 +0530

    modified

--- src/callback.js
+++ src/callback.js
@@ -1,26 +1,39 @@
 import React, { useState, useCallback } from "react";
+const functions = new Set();
 
-function Callback() {
-  const [state, setState] = useState(0);
-  const [val, setValue] = useState("");
+export default function Callback() {
+  const [counter, setCounter] = useState(0);
+  const [demo, setDemo] = useState(0);
 
-  const callbackText = useCallback((event) => {
-    setValue(event.target.val);
-  }, [val]);
+  const callbackButton1 = useCallback(() => {
+    setCounter(counter + 10);
+    console.log("Counter:", counter);
+  }, [counter]);
 
-  const callbackButton = useCallback(() => {
-    setState(state + 1);
-    console.log("callbackButton:", callbackButton);
-  }, [state]);
+  const callbackButton2 = useCallback(() => {
+    setCounter(counter - 10);
+    console.log("Counter:", counter);
+  }, [counter]);
+
+  const Test = useCallback(() => {
+    setDemo(demo + 1);
+    console.log("Demo:", demo);
+  }, [demo]);
+
+  functions.add(callbackButton1);
+  functions.add(callbackButton2);
+  functions.add(Test);
+  console.log("Functions:", functions.size);
 
   return (
     <div>
       <p>"useCallback demo"</p>
-      <input type="text" value={val} onChange={callbackText} />
-      <button onClick={callbackButton}>CallBack</button>
-      <p>{state}</p>
+      <input type="text" defaultValue={counter || ""} />
+      <br />
+      <button onClick={callbackButton1}>Add</button>
+      <button onClick={callbackButton2}>Subtract</button>
+      <button onClick={Test}>demo</button>
+      <p>{demo}</p>
     </div>
   );
 }
-
-export default Callback;
