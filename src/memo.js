8ea78989204ef9ed5c1150a3d8ce79482ec1c8b2
Drashty Batavia <DrashtyB@viitorcloud.local>
2020-03-16 19:03:26 +0530

    recreated

--- src/memo.js
+++ src/memo.js
@@ -1,31 +1,68 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 
-function Memo() {
-  const [count1, setCount1] = useState(null);
-  const [count2, setCount2] = useState(null);
-  const multiplier = (count1, count2) => {
-    return count1 * count2;
-  };
-  const memoCount = useMemo(() => {
-    return multiplier(count1, count2);
-  }, [count1, count2]);
-  console.log('memoCount:', memoCount)
+const list = [10, 60, 40, 30, 90, 0];
+
+export default function Memo() {
+  const [count, setCount] = useState(0);
+
+  // Render only when a list change occurs, and if there is no change, use the memorized contents
+  const sort = useMemo(() => {
+    console.log("useMemo");
+    return list.sort((a, b) => a - b);
+  }, [list]);
+
+  // When used in general, it works very well in Re-render.
+  const normalSort = list.sort((a, b) => a - b);
+
+  useEffect(() => {
+    console.log("re-render");
+  });
+
   return (
     <div>
-      <p>"useMemo:"</p>
-      Count: {memoCount}
+      <h3>useMemo Sort</h3>
+      {sort.map((item, i) => (
+        <li key={i}>{item}</li>
+      ))}
+      <h3>Normal Sort</h3>
+      {normalSort.map((item, i) => (
+        <li key={i}>{item}</li>
+      ))}
       <br />
-      <input
-        type="text"
-        onChange={e => setCount1(e.target.value)}
-        placeholder="Count 1"
-      />
-      <input
-        type="text"
-        onChange={e => setCount2(e.target.value)}
-        placeholder="Count 2"
-      />
+      <button onClick={() => setCount(count + 1)}>Re Render</button>
     </div>
   );
 }
-export default Memo;
+
+// export default function Memo() {
+//   const [count1, setCount1] = useState(null);
+//   const [count2, setCount2] = useState(null);
+
+//   const multiplier = (count1, count2) => {
+//     return count1 * count2;
+//   };
+//   const memoCount = useMemo(() => {
+//     return multiplier(count1, count2);
+//   }, [count1]);
+
+//   console.log("memoCount:", memoCount);
+
+//   return (
+//     <div>
+//       <p>"useMemo:"</p>
+//       Count: {memoCount}
+//       <br />
+//       <input
+//         type="text"
+//         onChange={e => setCount1(e.target.value)}
+//         placeholder="Count 1"
+//       />
+//       <br/>
+//       <input
+//         type="text"
+//         onChange={e => setCount2(e.target.value)}
+//         placeholder="Count 2"
+//       />
+//     </div>
+//   );
+// }
